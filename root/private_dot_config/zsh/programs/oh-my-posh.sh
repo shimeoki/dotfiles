42f31d680a1da7fa0404539c7682657e07145e7b
shimeoki <shimeoki@gmail.com>
2024-10-02 00:49:21 +0300

    feat(oh-my-posh): add config and use it

--- root/private_dot_config/zsh/programs/oh-my-posh.sh
+++ root/private_dot_config/zsh/programs/oh-my-posh.sh
@@ -2,6 +2,6 @@
 
 # enable oh-my-posh
 # source: https://ohmyposh.dev/docs/installation/prompt
-OMP_CONFIG="$OMP_CONFIG_HOME/zen.toml"
+OMP_CONFIG="$OMP_CONFIG_HOME/main.yaml"
 eval "$(oh-my-posh init zsh --config $OMP_CONFIG)"
 
