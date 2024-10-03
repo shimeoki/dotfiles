27246f5a8a824c4a703611e93a44cf888f716bac
shimeoki <shimeoki@gmail.com>
2024-10-03 11:19:16 +0300

    feat(zsh): add more aliases

--- root/private_dot_config/zsh/rc/aliases.sh
+++ root/private_dot_config/zsh/rc/aliases.sh
@@ -2,16 +2,20 @@
 
 export ZSH_RC_ALIASES="$ZSH_RC/aliases"
 
+source "$ZSH_RC_ALIASES/bat.sh"
 source "$ZSH_RC_ALIASES/chezmoi.sh"
+source "$ZSH_RC_ALIASES/clear.sh"
 source "$ZSH_RC_ALIASES/env.sh"
 source "$ZSH_RC_ALIASES/eza.sh"
 source "$ZSH_RC_ALIASES/fastfetch.sh"
+source "$ZSH_RC_ALIASES/fzf.sh"
 source "$ZSH_RC_ALIASES/git.sh"
 source "$ZSH_RC_ALIASES/grep.sh"
 source "$ZSH_RC_ALIASES/helix.sh"
+source "$ZSH_RC_ALIASES/kitty.sh"
+source "$ZSH_RC_ALIASES/lazygit.sh"
 source "$ZSH_RC_ALIASES/nvim.sh"
 source "$ZSH_RC_ALIASES/pacman.sh"
 source "$ZSH_RC_ALIASES/sudo.sh"
 source "$ZSH_RC_ALIASES/waybar.sh"
-source "$ZSH_RC_ALIASES/kitty.sh"
 
---
+++ root/private_dot_config/zsh/rc/aliases/lazygit.sh
@@ -0,0 +1,4 @@
+#!/bin/env sh
+
+alias lg='lazygit'
+
