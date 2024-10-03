20e9ce537aed4bd9a5fa97386ebf7ed215e3d282
shimeoki <shimeoki@gmail.com>
2024-10-03 12:17:10 +0300

    feat(zsh): add more git aliases

--- root/private_dot_config/zsh/rc/aliases/git.sh
+++ root/private_dot_config/zsh/rc/aliases/git.sh
@@ -3,9 +3,13 @@
 # git
 alias g='git'
 alias ga='git add'
+alias gr='git reset'
 alias gs='git status'
 alias gd='git diff'
 alias gl='git log'
 alias gc='git commit'
 alias gp='git push'
+alias gb='git branch'
+alias gsw='git switch'
+alias gre='git restore'
 
