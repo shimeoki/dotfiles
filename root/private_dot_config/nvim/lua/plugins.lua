069b4783fc7eaffa3b237a11ff7a2da7ee1a3052
shimeoki <shimeoki@gmail.com>
2025-01-13 02:49:16 +0300

    feat(treesitter): set initial config

--- root/private_dot_config/nvim/lua/plugins.lua
+++ root/private_dot_config/nvim/lua/plugins.lua
@@ -6,6 +6,21 @@ return {
 	},
 	{
 		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate"
+		build = ":TSUpdate",
+		config = function ()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				ensure_installed = {
+					"lua"
+				},
+				sync_install = false,
+				highlight = {
+					enable = true
+				},
+				indent = {
+					enable = true
+				},
+			})
+		end
 	},
 }
