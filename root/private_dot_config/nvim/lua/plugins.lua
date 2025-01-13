1edff043de47e99924311fe8515bf1a256880554
shimeoki <shimeoki@gmail.com>
2025-01-14 01:46:39 +0300

    refactor(nvim): move catppuccin to plugins/

--- root/private_dot_config/nvim/lua/plugins.lua
+++ root/private_dot_config/nvim/lua/plugins.lua
@@ -1,10 +1,5 @@
 return {
     {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000
-    },
-    {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         config = function ()
---
+++ root/private_dot_config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+table.insert(M, {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000
+})
+
+return M
