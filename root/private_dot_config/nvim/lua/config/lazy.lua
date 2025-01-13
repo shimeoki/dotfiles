10954989863fc549047d328f6feb132905cce699
shimeoki <shimeoki@gmail.com>
2025-01-13 03:21:50 +0300

    style(nvim): convert tabs to spaces

--- root/private_dot_config/nvim/lua/config/lazy.lua
+++ root/private_dot_config/nvim/lua/config/lazy.lua
@@ -3,19 +3,19 @@
 local path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(path) then
-	local repo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", repo, path })
+    local repo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", repo, path })
 
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\npress any key to exit" },
-		}, true, {})
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\npress any key to exit" },
+        }, true, {})
 
-		vim.fn.getchar()
-		os.exit(1)
-	end
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 
 vim.opt.rtp:prepend(path)
@@ -24,18 +24,18 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 require("lazy").setup({
-	spec = {
-		{
-			import = "plugins"
-		},
-	},
-	install = {
-		missing = true,
-		colorscheme = {
-			"catppuccin"
-		}
-	},
-	checker = {
-		enabled = true
-	},
+    spec = {
+        {
+            import = "plugins"
+        },
+    },
+    install = {
+        missing = true,
+        colorscheme = {
+            "catppuccin"
+        }
+    },
+    checker = {
+        enabled = true
+    },
 })
--- root/private_dot_config/nvim/lua/plugins.lua
+++ root/private_dot_config/nvim/lua/plugins.lua
@@ -1,26 +1,26 @@
 return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function ()
-			local configs = require("nvim-treesitter.configs")
-			configs.setup({
-				ensure_installed = {
-					"lua"
-				},
-				sync_install = false,
-				highlight = {
-					enable = true
-				},
-				indent = {
-					enable = true
-				},
-			})
-		end
-	},
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function ()
+            local configs = require("nvim-treesitter.configs")
+            configs.setup({
+                ensure_installed = {
+                    "lua"
+                },
+                sync_install = false,
+                highlight = {
+                    enable = true
+                },
+                indent = {
+                    enable = true
+                },
+            })
+        end
+    },
 }
