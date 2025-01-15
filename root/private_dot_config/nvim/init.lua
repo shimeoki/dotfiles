d7aa251ab85bae5105c978026f43760c8359b2ce
shimeoki <shimeoki@gmail.com>
2025-01-16 00:04:13 +0300

    refactor(nvim): separate options file

--- root/private_dot_config/nvim/init.lua
+++ root/private_dot_config/nvim/init.lua
@@ -1,13 +1,8 @@
 require("binds")
+require("options")
 
 require("config.lazy")
 require("config.treesitter")
 
 -- theme
 vim.cmd.colorscheme "catppuccin-mocha"
-
--- tabs
-vim.opt.tabstop = 8
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
---
+++ root/private_dot_config/nvim/lua/options.lua
@@ -0,0 +1,6 @@
+-- tabs
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
