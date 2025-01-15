5bb1216ff63c0c624c4d2f473da7858ffd1c735d
shimeoki <shimeoki@gmail.com>
2025-01-16 01:47:31 +0300

    feat(nvim): add wrapping
    80 columns wide by default

--- root/private_dot_config/nvim/lua/options.lua
+++ root/private_dot_config/nvim/lua/options.lua
@@ -1,6 +1,8 @@
 local tab_width = 4
 local tab_to_spaces = true
 
+local columns = 80
+
 local set = vim.opt
 
 set.tabstop = 8
@@ -11,3 +13,9 @@ set.expandtab = tab_to_spaces
 set.number = true
 set.relativenumber = true
 
+-- source: https://stackoverflow.com/a/50415982
+set.textwidth = 0
+set.wrapmargin = 0
+set.wrap = true
+set.linebreak = true
+set.columns = columns
