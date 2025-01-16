54b8b908a2ff49799ac0532c78152686f0dbeceb
shimeoki <shimeoki@gmail.com>
2025-01-17 00:58:19 +0300

    style(nvim): cfg variable in treesitter

--- root/private_dot_config/nvim/lua/plugins/treesitter.lua
+++ root/private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,8 @@
+local cfg = require("config.treesitter")
+
 return {
 	"nvim-treesitter/nvim-treesitter",
 	main = "nvim-treesitter.configs",
 	build = ":TSUpdate",
-	opts = require("config.treesitter").opts,
+	opts = cfg.opts,
 }
