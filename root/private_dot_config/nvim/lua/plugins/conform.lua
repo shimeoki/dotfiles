0da0b6d8c5c326444a6db3bd70f4edcaef3a64ed
shimeoki <shimeoki@gmail.com>
2025-01-17 00:55:37 +0300

    feat(nvim): use lazy loading for conform

--- root/private_dot_config/nvim/lua/plugins/conform.lua
+++ root/private_dot_config/nvim/lua/plugins/conform.lua
@@ -2,5 +2,8 @@ local cfg = require("config.conform")
 
 return {
 	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
 	opts = cfg.opts,
+	init = cfg.set_formatexpr,
 }
