dae1b82a8df3afe05a201db2fcf3b2c1a97aabb3
shimeoki <shimeoki@gmail.com>
2025-01-18 16:28:53 +0300

    feat(nvim): add langmap to options

--- root/private_dot_config/nvim/lua/options.lua
+++ root/private_dot_config/nvim/lua/options.lua
@@ -24,3 +24,19 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 set.termguicolors = true
+
+-- source: https://github.com/Wansmer/langmapper.nvim
+local function escape(str)
+	local escape_chars = [[;,."|\]]
+	return vim.fn.escape(str, escape_chars)
+end
+
+local en = [[`qwertyuiop[]asdfghjkl;'zxcvbnm]]
+local ru = [[ёйцукенгшщзхъфывапролджэячсмить]]
+local en_shift = [[~QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>]]
+local ru_shift = [[ËЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ]]
+
+set.langmap = vim.fn.join({
+	escape(ru_shift) .. ";" .. escape(en_shift),
+	escape(ru) .. ";" .. escape(en),
+}, ",")
