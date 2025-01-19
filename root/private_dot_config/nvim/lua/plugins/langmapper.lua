27c99853ff885afa2aadda2dc9a7cda528cbe734
shimeoki <shimeoki@gmail.com>
2025-01-19 20:55:59 +0300

    refactor(nvim): move config to plugins/

--- root/private_dot_config/nvim/lua/plugins/langmapper.lua
+++ root/private_dot_config/nvim/lua/plugins/langmapper.lua
@@ -1,9 +1,22 @@
-local cfg = require("config.langmapper")
+local name = "langmapper"
+local enabled = true
+
+local function config()
+	local plug = require(name)
+
+	plug.automapping({
+		global = true,
+		buffer = true,
+	})
+end
 
 return {
 	"Wansmer/langmapper.nvim",
+	cond = enabled,
+	name = name,
+	main = name,
 	lazy = false,
 	priority = 1,
 	opts = {},
-	config = cfg.init,
+	config = config,
 }
