16c8d025d087e4b73b64ac1286f0d1f9e62f9a69
shimeoki <shimeoki@gmail.com>
2025-01-22 22:59:35 +0300

    feat(nvim): advanced cmp binds and appearance

--- root/private_dot_config/nvim/lua/config/cmp.lua
+++ root/private_dot_config/nvim/lua/config/cmp.lua
@@ -4,6 +4,10 @@ local function plug()
 	return require("cmp")
 end
 
+local function luasnip()
+	return require("luasnip")
+end
+
 local snippet = {
 	expand = function(args)
 		require("luasnip").lsp_expand(args.body)
@@ -12,15 +16,45 @@ local snippet = {
 
 -- todo move to binds
 function M.binds()
-	local map = plug().mapping
+	local cmp = plug()
+	local snip = luasnip()
+	local map = cmp.mapping
 
-	return map.preset.insert({
-		["<C-b>"] = map.scroll_docs(-4),
-		["<C-f>"] = map.scroll_docs(4),
-		["<C-Space>"] = map.complete(),
-		["<C-e>"] = map.abort(),
-		["<CR>"] = map.confirm({ select = true }),
-	})
+	local function jump(step)
+		return function(fallback)
+			if cmp.visible() then
+				if step > 0 then
+					cmp.select_next_item()
+				else
+					cmp.select_prev_item()
+				end
+			elseif snip.locally_jumpable(step) then
+				snip.jump(step)
+			else
+				fallback()
+			end
+		end, { "i", "s" }
+	end
+
+	local function select()
+		return function(fallback)
+			if cmp.visible() then
+				if snip.expandable() then
+					snip.expand()
+				else
+					cmp.confirm({ select = true })
+				end
+			else
+				fallback()
+			end
+		end
+	end
+
+	return {
+		["<cr>"] = map(select()),
+		["<tab>"] = map(jump(1)),
+		["<s-tab>"] = map(jump(-1)),
+	}
 end
 
 function M.src()
@@ -30,6 +64,20 @@ function M.src()
 	}, {
 		{ name = "buffer" },
 	})
+end
+
+function M.window()
+	local bordered = plug().config.window.bordered()
+
+	return {
+		completion = bordered,
+		documentation = bordered,
+	}
+end
+
+function M.autopairs_setup()
+	local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+	plug().event:on("confirm_done", cmp_autopairs.on_confirm_done())
 end
 
 M.opts = {
