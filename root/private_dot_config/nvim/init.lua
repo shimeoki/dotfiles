536a49655df5e339e17097f49b8105811f21d310
shimeoki <shimeoki@gmail.com>
2025-01-22 23:01:16 +0300

    feat(nvim): add rounded border to diagnostic

--- root/private_dot_config/nvim/init.lua
+++ root/private_dot_config/nvim/init.lua
@@ -2,5 +2,10 @@ require("binds")
 require("options")
 require("plugman")
 
+-- todo move somewhere else
+vim.diagnostic.config({
+	float = { border = "rounded" },
+})
+
 -- theme
 vim.cmd.colorscheme("catppuccin-mocha")
