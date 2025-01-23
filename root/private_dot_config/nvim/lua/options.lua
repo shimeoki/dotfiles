d6029bf10ceb0e7bc0b2c17922c46e5234bc53f5
shimeoki <shimeoki@gmail.com>
2025-01-23 23:29:24 +0300

    feat(nvim): add more options
    fix that annoying signcolumn jumping

--- root/private_dot_config/nvim/lua/options.lua
+++ root/private_dot_config/nvim/lua/options.lua
@@ -1,7 +1,7 @@
 local tab_width = 4
 local tab_to_spaces = true
-
 local columns = 80
+local scrolloff = 4
 
 local set = vim.opt
 
@@ -24,6 +24,18 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 set.termguicolors = true
+
+set.scrolloff = scrolloff
+
+set.signcolumn = "yes"
+
+set.hlsearch = false
+set.incsearch = true
+set.ignorecase = true
+set.smartcase = true
+
+set.showmode = false
+set.showtabline = 0
 
 -- source: https://github.com/Wansmer/langmapper.nvim
 local function escape(str)
