fd169600dd93c42f7ffcd2c351e740aa36e735ce
shimeoki <shimeoki@gmail.com>
2025-01-23 19:02:12 +0300

    perf(nvim): lazy load treesitter

--- root/private_dot_config/nvim/lua/plugins/treesitter.lua
+++ root/private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -8,10 +8,20 @@ local opts = {
 	},
 }
 
+-- source: https://www.lazyvim.org/plugins/treesitter
 return {
 	"nvim-treesitter/nvim-treesitter",
+	name = "treesitter",
 	main = "nvim-treesitter.configs",
+	version = false, -- last release doesn't work on windows
 	cond = cfg.syntax_enabled,
+	event = { "BufReadPost", "BufWritePost", "BufNewFile", "VeryLazy" },
+	lazy = vim.fn.argc(-1) == 0,
+	init = function(plugin)
+		require("lazy.core.loader").add_to_rtp(plugin)
+		require("nvim-treesitter.query_predicates")
+	end,
+	cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
 	build = ":TSUpdate",
 	opts = opts,
 }
