b74619dcb77cfde1d58e974e75c0636952144670
shimeoki <shimeoki@gmail.com>
2025-01-25 17:35:00 +0300

    feat(nvim): add todo-comments config

--- root/private_dot_config/nvim/lua/plugins/todo_comments.lua
+++ root/private_dot_config/nvim/lua/plugins/todo_comments.lua
@@ -1,11 +1,67 @@
+local enabled = true
+
+local alts = {
+	FIX = {
+		"FIXME",
+		"BUG",
+		"FIXIT",
+		"ISSUE",
+	},
+	TODO = {},
+	HACK = {},
+	WARN = {
+		"WARNING",
+		"XXX",
+	},
+	PERF = {
+		"OPTIM",
+		"PERFORMANCE",
+		"OPTIMIZE",
+	},
+	NOTE = {
+		"INFO",
+	},
+	TEST = {
+		"TESTING",
+		"PASSED",
+		"FAILED",
+	},
+}
+
+local keywords = {
+	FIX = { icon = " ", color = "error" },
+	TODO = { icon = " ", color = "info" },
+	HACK = { icon = " ", color = "warning" },
+	WARN = { icon = " ", color = "warning" },
+	PERF = { icon = " ", color = "info" },
+	NOTE = { icon = " ", color = "info" },
+	TEST = { icon = "⏲ ", color = "test" },
+}
+
+for k, v in pairs(alts) do
+	local alt = {}
+	table.insert(alt, k:lower())
+
+	for _, s in ipairs(v) do
+		table.insert(alt, s)
+		table.insert(alt, s:lower())
+	end
+
+	keywords[k].alt = alt
+end
+
 return {
 	"folke/todo-comments.nvim",
+	cond = enabled,
 	name = "todo-comments",
 	main = "todo-comments",
 	cmd = { "TodoTelescope" },
 	event = { "BufReadPost", "BufWritePost", "BufNewFile" },
 	opts = {
 		signs = false,
+		keywords = keywords,
 	},
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
 }
