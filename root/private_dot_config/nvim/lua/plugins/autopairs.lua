cb30b225a41ddf6a9bf816da83934916a6539966
shimeoki <shimeoki@gmail.com>
2025-01-29 20:33:59 +0300

    feat(nvim): plugins.autopairs

--- root/private_dot_config/nvim/lua/plugins/autopairs.lua
+++ root/private_dot_config/nvim/lua/plugins/autopairs.lua
@@ -1,5 +1,11 @@
+local enabled = true
+local name = "autopairs"
+
 return {
 	"windwp/nvim-autopairs",
+	cond = enabled,
+	name = name,
+	main = "nvim-autopairs",
 	event = "InsertEnter",
 	opts = {},
 }
