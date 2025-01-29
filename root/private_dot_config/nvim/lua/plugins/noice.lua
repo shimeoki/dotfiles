61bcb74c2e92debd51d5a466258cc9081e3bea30
shimeoki <shimeoki@gmail.com>
2025-01-29 21:16:32 +0300

    refactor(nvim): split noice config

--- root/private_dot_config/nvim/lua/plugins/noice.lua
+++ root/private_dot_config/nvim/lua/plugins/noice.lua
@@ -1,20 +1,15 @@
+local enabled = true
+local name = "noice"
+
+local noice = require("config.plugins.noice")
+
 return {
 	"folke/noice.nvim",
+	cond = enabled,
+	name = name,
+	main = name,
 	event = "VeryLazy",
-	opts = {
-		lsp = {
-			override = {
-				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-				["vim.lsp.util.stylize_markdown"] = true,
-				["cmp.entry.get_documentation"] = true,
-			},
-		},
-		presets = {
-			command_palette = true,
-			long_message_to_split = true,
-			lsp_doc_border = true,
-		},
-	},
+	opts = noice.opts,
 	dependencies = {
 		"MunifTanjim/nui.nvim",
 		"rcarriga/nvim-notify",
