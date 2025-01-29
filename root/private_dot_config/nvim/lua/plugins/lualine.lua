86eaf874e1d8c315d1704a48e1e40cc060e164dc
shimeoki <shimeoki@gmail.com>
2025-01-29 20:14:23 +0300

    refactor(nvim): split lualine config

--- root/private_dot_config/nvim/lua/plugins/lualine.lua
+++ root/private_dot_config/nvim/lua/plugins/lualine.lua
@@ -1,123 +1,14 @@
-local options = {
-	component_separators = "",
-}
+local enabled = true
+local name = "lualine"
 
-local position = {
-	{
-		"mode",
-		fmt = function(s)
-			return string.sub(string.lower(s), 1, 3)
-		end,
-	},
-	{ "location" },
-	{
-		"progress",
-		fmt = string.lower,
-	},
-}
-
-local fileinfo = {
-	{
-		"fileformat",
-		symbols = {
-			unix = "lf",
-			dos = "crlf",
-			mac = "lf",
-		},
-	},
-	{
-		"encoding",
-		show_bomb = false,
-	},
-}
-
-local file = {
-	{
-		"filename",
-		file_status = true,
-		newfile_status = true,
-		path = 4,
-		shorting_target = 40,
-		symbols = {
-			modified = "~",
-			readonly = "=",
-			unnamed = "?",
-			newfile = "+",
-		},
-	},
-	{
-		"filetype",
-		colored = true,
-		icon_only = false,
-		icon = { align = "left" },
-	},
-}
-
-local git = {
-	{
-		"diff",
-		colored = true,
-		symbols = {
-			added = "+",
-			modified = "~",
-			removed = "-",
-		},
-	},
-	{
-		"branch",
-		icon = "@",
-	},
-}
-
-local status = {
-	{
-		"diagnostics",
-		sources = {
-			"nvim_lsp",
-			"nvim_diagnostic",
-		},
-		sections = {
-			"error",
-			"warn",
-			"info",
-			"hint",
-		},
-		colored = true,
-		update_in_insert = false,
-		always_visible = false,
-	},
-}
-
-local active_sections = {
-	lualine_a = position,
-	lualine_b = fileinfo,
-	lualine_c = file,
-	lualine_x = status,
-	lualine_y = git,
-	lualine_z = {},
-}
-
-local inactive_sections = {
-	lualine_a = {},
-	lualine_b = {},
-	lualine_c = file,
-	lualine_x = status,
-	lualine_y = {},
-	lualine_z = {},
-}
+local lualine = require("config.plugins.lualine")
 
 return {
 	"nvim-lualine/lualine.nvim",
-	cond = true,
-	name = "lualine",
-	main = "lualine",
+	cond = enabled,
+	name = name,
+	main = name,
 	event = "VeryLazy",
-	opts = {
-		options = options,
-		sections = active_sections,
-		inactive_sections = inactive_sections,
-	},
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
+	opts = lualine.opts,
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 }
