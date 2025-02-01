db7361e16817de4fdb04f00cc579fd79eaef26c6
shimeoki <shimeoki@gmail.com>
2025-02-02 01:00:05 +0300

    feat(nvim): split telescope config
    and add more options

---
+++ root/private_dot_config/nvim/lua/config/plugins/telescope.lua
@@ -0,0 +1,72 @@
+local M = {}
+
+local fuzzy = true
+
+local sorting_strategy = "ascending"
+local layout_strategy = "horizontal"
+local layout_config = {
+	anchor = "CENTER",
+	height = { padding = 2 },
+	mirror = false,
+	prompt_position = "top",
+	scroll_speed = 4,
+	width = { padding = 4 },
+	preview_cutoff = 100,
+	preview_width = 80,
+}
+
+local mappings = {
+	i = {
+		["<c-j>"] = "move_selection_next",
+		["<c-k>"] = "move_selection_previous",
+		["J"] = "preview_scrolling_down",
+		["K"] = "preview_scrolling_up",
+	},
+	n = {
+		["J"] = "preview_scrolling_down",
+		["K"] = "preview_scrolling_up",
+	},
+}
+
+local fzf = {
+	fuzzy = true,
+	override_generic_sorter = true,
+	override_file_sorter = true,
+	case_mode = "smart_case",
+}
+
+M.opts = {
+	defaults = {
+		layout_strategy = layout_strategy,
+		layout_config = layout_config,
+		sorting_strategy = sorting_strategy,
+		mappings = mappings,
+	},
+}
+
+if fuzzy then
+	M.opts.extensions = { fzf = fzf }
+end
+
+local binds = require("config.binds")
+M.keys = binds.convert(binds.map.telescope, "lazy")
+
+-- enable line wrapping in preview
+vim.api.nvim_create_autocmd("User", {
+	pattern = "TelescopePreviewerLoaded",
+	callback = function()
+		vim.wo.wrap = true
+	end,
+})
+
+function M.config()
+	local telescope = require("telescope")
+
+	telescope.setup(M.opts)
+
+	if fuzzy then
+		telescope.load_extension("fzf")
+	end
+end
+
+return M
--- root/private_dot_config/nvim/lua/plugins/telescope.lua
+++ root/private_dot_config/nvim/lua/plugins/telescope.lua
@@ -2,36 +2,19 @@ local enabled = true
 
 local sorter = {
 	"nvim-telescope/telescope-fzf-native.nvim",
+	name = "telescope-fzf-native",
 	build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
 }
 
-local binds = require("config.binds")
-local keys = binds.convert(binds.map.telescope, "lazy")
+local telescope = require("config.plugins.telescope")
 
 return {
 	"nvim-telescope/telescope.nvim",
 	cond = enabled,
 	name = "telescope",
 	branch = "0.1.x",
-	keys = keys,
-	config = function()
-		local plug = require("telescope")
-
-		local fzf = {
-			fuzzy = true,
-			override_generic_sorter = true,
-			override_file_sorter = true,
-			case_mode = "smart_case",
-		}
-
-		plug.setup({
-			extensions = {
-				fzf = fzf,
-			},
-		})
-
-		plug.load_extension("fzf")
-	end,
+	keys = telescope.keys,
+	config = telescope.config,
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		sorter,
