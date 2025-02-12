9b7bc22ac6d45a37703ff3118b73be49fb93d498
shimeoki <shimeoki@gmail.com>
2025-02-12 19:10:12 +0300

    feat(nvim): config.plugins import optimization

--- root/private_dot_config/nvim/lua/config/plugins.lua
+++ root/private_dot_config/nvim/lua/config/plugins.lua
@@ -7,93 +7,97 @@ local M = {}
 --- @field enabled boolean
 --- @field config? table | fun(any): table
 
----@param plugin Plugin
+--- @param name string
+local function safe_require(name)
+	local ok, res = pcall(require, name)
+
+	if ok then
+		return res, nil
+	else
+		return nil, res
+	end
+end
+
+--- @param plugin Plugin
 local function add(plugin)
+	if not plugin.config and plugin.enabled then
+		local module_name = "config.plugins." .. plugin.name
+		local module = safe_require(module_name)
+
+		plugin.config = module or {}
+	end
+
 	M[plugin.name] = plugin
 end
 
 add({
 	name = "catppuccin",
 	enabled = true,
-	config = require("config.plugins.catppuccin"),
 })
 
 add({
 	name = "cmp",
 	enabled = true,
-	config = require("config.plugins.cmp"),
 })
 
 add({
 	name = "conform",
 	enabled = true,
-	config = require("config.plugins.conform"),
 })
 
 add({
 	name = "jdlts",
 	enabled = true,
-	config = require("config.plugins.jdtls"),
 })
 
 add({
 	name = "langmapper",
 	enabled = true,
-	config = require("config.plugins.langmapper"),
 })
 
 add({
 	name = "lualine",
 	enabled = true,
-	config = require("config.plugins.lualine"),
 })
 
 add({
 	name = "mason",
 	enabled = true,
-	config = require("config.plugins.mason"),
 })
 
 add({
 	name = "noice",
 	enabled = true,
-	config = require("config.plugins.noice"),
 })
 
 add({
 	name = "telescope",
 	enabled = true,
-	config = require("config.plugins.telescope"),
 })
 
 add({
 	name = "todo_comments",
 	enabled = true,
-	config = require("config.plugins.todo_comments"),
 })
 
 add({
 	name = "treesitter",
 	enabled = true,
-	config = require("config.plugins.treesitter"),
 })
 
 add({
 	name = "which_key",
 	enabled = true,
-	config = require("config.plugins.which_key"),
 })
 
 add({
 	name = "lint",
 	enabled = true,
-	config = require("config.plugins.lint"),
 })
 
 add({
 	name = "lspconfig",
 	enabled = true,
-	config = require("config.plugins.lspconfig"),
 })
 
 return M
