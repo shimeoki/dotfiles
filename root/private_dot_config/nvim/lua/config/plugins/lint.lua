4c527b6476903a7491e70f1dd1e331f9d3a5c3bc
shimeoki <shimeoki@gmail.com>
2025-02-13 22:52:43 +0300

    fix(nvim): add linters_by_ft to nvim-lint

--- root/private_dot_config/nvim/lua/config/plugins/lint.lua
+++ root/private_dot_config/nvim/lua/config/plugins/lint.lua
@@ -9,17 +9,20 @@ function M.setup()
 
 	local lint = require("lint")
 
-	vim.api.nvim_create_autocmd(linters.opts.events, {
-		callback = function()
-			lint.try_lint()
-		end,
-	})
+	lint.linters_by_ft = linters.by_filetype
 
 	for _, linter in ipairs(linters.enabled) do
 		if linter.config then
 			lint.linters[linter.name].args = linter.config.args
 		end
 	end
+
+	vim.api.nvim_create_autocmd(linters.opts.events, {
+		group = vim.api.nvim_create_augroup("lint", { clear = true }),
+		callback = function()
+			lint.try_lint()
+		end,
+	})
 end
 
 return M
