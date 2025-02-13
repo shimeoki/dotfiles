5c5c46e0e9dc8e0bb255160ec6871f6a75f54952
shimeoki <shimeoki@gmail.com>
2025-02-13 22:50:58 +0300

    feat(nvim): add ruff (as a formatter)

--- root/private_dot_config/nvim/lua/config/formatters.lua
+++ root/private_dot_config/nvim/lua/config/formatters.lua
@@ -15,6 +15,35 @@ M:add({
 	filetypes = { "go" },
 })
 
+-- fixme: multiple names support?
+
+M:add({
+	name = "ruff_fix",
+	enabled = true,
+	filetypes = { "python" },
+	aliases = {
+		mason = "ruff",
+	},
+})
+
+M:add({
+	name = "ruff_format",
+	enabled = true,
+	filetypes = { "python" },
+	aliases = {
+		mason = "ruff",
+	},
+})
+
+M:add({
+	name = "ruff_organize_imports",
+	enabled = true,
+	filetypes = { "python" },
+	aliases = {
+		mason = "ruff",
+	},
+})
+
 M:parse_groups({ "mason" })
 
 return M
