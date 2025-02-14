2fa7d09b02517e6bce8adf5125dc94a0ce3cfdf8
shimeoki <shimeoki@gmail.com>
2025-02-14 16:33:04 +0300

    feat(nvim): add image.nvim opts

--- root/private_dot_config/nvim/lua/plugins/image.lua
+++ root/private_dot_config/nvim/lua/plugins/image.lua
@@ -1,6 +1,16 @@
+-- todo: move opts
+
 return {
 	"3rd/image.nvim",
 	name = "image",
 	cond = true,
-	opts = {},
+	opts = {
+		backend = "kitty",
+		max_width = 128,
+		max_height = 16,
+		max_height_window_percentage = math.huge,
+		max_width_window_percentage = math.huge,
+		window_overlap_clear_enabled = true,
+		window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+	},
 }
