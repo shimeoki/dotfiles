2d1eebdfe95d2b107d64e9575c31b540bf24a304
shimeoki <shimeoki@gmail.com>
2025-02-18 14:19:35 +0300

    feat(nvim): add quarto

---
+++ root/private_dot_config/nvim/lua/plugman/lazy/spec/quarto.lua
@@ -0,0 +1,33 @@
+-- fix: separate
+
+return {
+	"quarto-dev/quarto-nvim",
+	name = "quarto",
+	ft = { "markdown" },
+	dependencies = {
+		"jmbuhr/otter.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	opts = {
+		lspFeatures = {
+			languages = { "python" },
+			chunks = "all",
+			diagnostics = {
+				enabled = true,
+				triggers = { "BufWritePost" },
+			},
+			completion = { enabled = true },
+		},
+		keymap = {
+			hover = "H",
+			definition = "gd",
+			rename = "<leader>rn",
+			references = "gr",
+			format = "<leader>gf",
+		},
+		codeRunner = {
+			enabled = true,
+			default_method = "molten",
+		},
+	},
+}
