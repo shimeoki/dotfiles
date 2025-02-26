21d6fa7ef3c44052af5896db9ddb0bee69d2c167
shimeoki <shimeoki@gmail.com>
2025-02-26 10:13:36 +0300

    fix(zsh): aliases styling and escaping

--- root/private_dot_config/zsh/rc/aliases.sh
+++ root/private_dot_config/zsh/rc/aliases.sh
@@ -12,17 +12,14 @@ alias cza='chezmoi apply'
 alias c='clear'
 
 # env
-alias e="$EDITOR"
-alias v="$VISUAL"
+alias e='$EDITOR'
+alias v='$VISUAL'
 
 # eza
-# grid
 alias l='eza'
 alias la='eza -a'
-# list
 alias ll='eza -l'
 alias lla='eza -la'
-# tree
 alias lt='eza -T'
 alias lta='eza -Ta'
 alias ltl='eza_tree'
@@ -33,64 +30,37 @@ alias ff='fastfetch'
 # fzf
 alias f='fzf'
 
-# git start
-
+# git
 alias g='git'
-
-# add
 alias ga='git add'
-
-# status
 alias gg='git status'
-
-# switch
 alias gs='git switch'
 alias gsc='git switch -c'
-
-# restore
 alias gr='git restore'
-
-# branch
 alias gb='git branch'
 alias gba='git branch --all'
 alias gbm='git branch --move'
 alias gbd='git branch --delete'
-
-# log
 alias gl='git log'
 alias gls='git shortlog'
 alias glg='git log --graph'
 alias gla='git log --all'
 alias glga='git log --graph --all'
-
-# diff
 alias gd='git diff'
 alias gdc='git diff --cached'
 alias gdh='git diff HEAD'
-
-# commit
 alias gc='git commit'
 alias gca='git commit --all'
-
-# push
 alias gp='git push'
 alias gpa='git push --all'
 alias gpt='git push --tags'
-
-# tag
 alias gt='git tag'
 alias gtn='git tag -n'
 alias gta='git tag --annotate'
 alias gtd='git tag --delete'
-
-# merge
 alias gm='git merge'
 alias gmf='git merge --ff'
-
-# fetch
 alias gf='git fetch'
-
-# git end
 
 # helix
 alias hx='helix'
@@ -109,8 +79,7 @@ alias nv='nvim'
 alias pm='pacman'
 
 # sudo
-alias sudo='sudo '
-# blank space is a hack
+alias sudo='sudo ' # blank space is a hack
 
 # waybar
 alias waybar-toggle='killall -SIGUSR1 waybar'
@@ -118,7 +87,6 @@ alias waybar-reload='killall -SIGUSR2 waybar'
 
 # swww
 alias sbg='swww img'
-# stands for "set bg"
 
 # nmcli
 alias wific='nmcli device wifi connect'
@@ -129,4 +97,3 @@ alias jj='j -'
 
 # gradle
 alias gw='./gradlew'
-
