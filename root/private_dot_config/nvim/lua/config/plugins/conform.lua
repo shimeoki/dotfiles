683b8822fac303429174343b11bd5b684ec28c20
shimeoki <shimeoki@gmail.com>
2025-03-01 18:47:52 +0300

    feat(nvim): add custom Format command

--- root/private_dot_config/nvim/lua/config/plugins/conform.lua
+++ root/private_dot_config/nvim/lua/config/plugins/conform.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+local api = vim.api
+
 local formatters = require("config.formatters")
 
 M.opts = {
@@ -12,6 +14,26 @@ if formatters.opts.on_save then
 		lsp_format = "fallback",
 	}
 end
+
+local function get_end(line2)
+	return api.nvim_buf_get_lines(0, line2 - 1, line2, true)[1]
+end
+
+vim.api.nvim_create_user_command("Format", function(args)
+	local range = nil
+	if args.count ~= -1 then
+		range = {
+			["start"] = { args.line1, 0 },
+			["end"] = { args.line2, get_end(args.line2):len() },
+		}
+	end
+
+	require("conform").format({
+		async = true,
+		lsp_format = "fallback",
+		range = range,
+	})
+end, { range = true })
 
 function M.init()
 	vim.o.formatexpr = 'v:lua.require("conform").formatexpr()'
