73276aacbc1b21e12a8c369b2aa57590e925f4dc
shimeoki <shimeoki@gmail.com>
2025-03-04 12:45:17 +0300

    feat(nvim): add separate silicon config

---
+++ root/private_dot_config/nvim/lua/config/plugins/silicon.lua
@@ -0,0 +1,36 @@
+local api = vim.api
+
+local directory = vim.env.HOME .. "/Pictures/screenshots/code/"
+local theme = vim.env.XDG_CONFIG_HOME .. "/bat/themes/Catppuccin Mocha.tmTheme"
+
+local window_title = function()
+	return vim.fn.fnamemodify(api.nvim_buf_get_name(api.nvim_get_current_buf()), ":t")
+end
+
+local output = function()
+	return directory .. os.date("!%Y-%m-%d-%H-%M-%S") .. ".png"
+end
+
+local opts = {
+	font = "FiraCode Nerd Font=16",
+	theme = theme,
+	pad_horiz = 0,
+	pad_vert = 0,
+	no_round_corner = true,
+	no_window_controls = true,
+	line_offset = function(args)
+		return args.line1
+	end,
+	tab_width = 4,
+	shadow_blur_radius = 0,
+	shadow_offset_x = 0,
+	shadow_offset_y = 0,
+	gobble = true,
+	to_clipboard = true,
+	window_title = window_title,
+	output = output,
+}
+
+return {
+	opts = opts,
+}
