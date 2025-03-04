efa95f1cfe4968f32835832589e0fde56b1e649e
shimeoki <shimeoki@gmail.com>
2025-03-04 12:45:44 +0300

    refactor(nvim): use separated silicon config

--- root/private_dot_config/nvim/lua/plugman/lazy/spec/silicon.lua
+++ root/private_dot_config/nvim/lua/plugman/lazy/spec/silicon.lua
@@ -1,40 +1,11 @@
--- todo: move config to separate file
-
-local api = vim.api
-
-local directory = vim.env.HOME .. "/Pictures/screenshots/code/"
-local theme = vim.env.XDG_CONFIG_HOME .. "/bat/themes/Catppuccin Mocha.tmTheme"
-
-local window_title = function()
-	return vim.fn.fnamemodify(api.nvim_buf_get_name(api.nvim_get_current_buf()), ":t")
-end
-
-local output = function()
-	return directory .. os.date("!%Y-%m-%d-%H-%M-%S") .. ".png"
-end
+local silicon = require("config.plugins").silicon
 
 return {
 	"michaelrommel/nvim-silicon",
+	cond = silicon.enabled,
+	name = silicon.name,
 	lazy = true,
 	main = "nvim-silicon",
 	cmd = "Silicon",
-	opts = {
-		font = "FiraCode Nerd Font=16",
-		theme = theme,
-		pad_horiz = 0,
-		pad_vert = 0,
-		no_round_corner = true,
-		no_window_controls = true,
-		line_offset = function(args)
-			return args.line1
-		end,
-		tab_width = 4,
-		shadow_blur_radius = 0,
-		shadow_offset_x = 0,
-		shadow_offset_y = 0,
-		gobble = true,
-		to_clipboard = true,
-		window_title = window_title,
-		output = output,
-	},
+	opts = silicon.config.opts,
 }
