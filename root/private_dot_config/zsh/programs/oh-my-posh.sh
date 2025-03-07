b7b391518fa903368d0bef73f1587e793fb13866
shimeoki <shimeoki@gmail.com>
2025-03-07 23:31:42 +0300

    fix(zsh): quotes in programs/oh-my-posh.sh
    to prevent globbing and splitting

--- root/private_dot_config/zsh/programs/oh-my-posh.sh
+++ root/private_dot_config/zsh/programs/oh-my-posh.sh
@@ -3,5 +3,4 @@
 # enable oh-my-posh
 # source: https://ohmyposh.dev/docs/installation/prompt
 OMP_CONFIG="$OMP_CONFIG_HOME/main.yaml"
-eval "$(oh-my-posh init zsh --config $OMP_CONFIG)"
-
+eval "$(oh-my-posh init zsh --config "$OMP_CONFIG")"
