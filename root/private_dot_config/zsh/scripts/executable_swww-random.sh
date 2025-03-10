560c1673b9bc4dca4be3dfc7b0b4fd4cb5214870
shimeoki <shimeoki@gmail.com>
2025-03-10 22:40:13 +0300

    feat(zsh): add swww-random script

---
+++ root/private_dot_config/zsh/scripts/executable_swww-random.sh
@@ -0,0 +1,22 @@
+#!/bin/env sh
+
+# modified (simplified) version of
+# https://github.com/LGFae/swww/blob/main/example_scripts/swww_randomize.sh
+
+# Changes the wallpaper to a randomly chosen image in a given directory
+# at a set interval.
+
+DEFAULT_INTERVAL=300 # seconds
+
+if [ $# -lt 1 ] || [ ! -d "$1" ]; then
+    echo "usage: <directory> [<interval>|300]"
+    return 0
+fi
+
+while true; do
+    fd . "$1" --type f | shuf |
+        while read -r img; do
+            swww img "$img"
+            sleep "${2:-$DEFAULT_INTERVAL}"
+        done
+done
