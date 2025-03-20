cd3491e5692e304a10968992279d3ad53e4ad059
shimeoki <shimeoki@gmail.com>
2025-03-20 13:15:17 +0300

    feat(nvim): add lazygit.nvim

---
+++ root/private_dot_config/nvim/lua/plugman/lazy/spec/lazygit.lua
@@ -0,0 +1,15 @@
+return {
+	"kdheepak/lazygit.nvim",
+	name = "lazygit",
+	cond = true,
+	cmd = {
+		"LazyGit",
+		"LazyGitConfig",
+		"LazyGitCurrentFile",
+		"LazyGitFilter",
+		"LazyGitFilterCurrentFile",
+	},
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+}
