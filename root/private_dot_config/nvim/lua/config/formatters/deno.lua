63c7ce9b3c80e6334c8459b44a8fb6cfa97c61c2
shimeoki <shimeoki@gmail.com>
2025-03-21 11:29:45 +0300

    feat(nvim): add denofmt config

---
+++ root/private_dot_config/nvim/lua/config/formatters/deno.lua
@@ -0,0 +1,10 @@
+return {
+	append_args = {
+		"--use-tabs=true",
+		"--indent-width=4",
+		"--line-width=80",
+		"--no-semicolons=false",
+		"--prose-wrap=always",
+		"--single-quote=false",
+	},
+}
