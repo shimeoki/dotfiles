0cadb968243f27209b9533ca5dba40b94eb7bd61
shimeoki <shimeoki@gmail.com>
2025-03-24 00:34:37 +0300

    feat(nvim): add csvview

---
+++ root/private_dot_config/nvim/lua/plugman/lazy/spec/csvview.lua
@@ -0,0 +1,18 @@
+return {
+	"hat0uma/csvview.nvim",
+	cond = true,
+	name = "csvview",
+	opts = {
+		parser = { comments = { "#", "//" } },
+		view = { display_mode = "border" },
+		keymaps = {
+			textobject_field_inner = { "if", mode = { "o", "x" } },
+			textobject_field_outer = { "af", mode = { "o", "x" } },
+			jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+			jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+			jump_next_row = { "<Enter>", mode = { "n", "v" } },
+			jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+		},
+	},
+	cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
+}
