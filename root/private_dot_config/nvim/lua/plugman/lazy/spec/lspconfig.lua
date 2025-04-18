4b8775cf1c73ccfa6e60ae3769e822eb6730d8e9
shimeoki <shimeoki@gmail.com>
2025-04-18 11:42:59 +0300

    refactor(nvim): lspconfig in lazy spec

--- root/private_dot_config/nvim/lua/plugman/lazy/spec/lspconfig.lua
+++ root/private_dot_config/nvim/lua/plugman/lazy/spec/lspconfig.lua
@@ -1,9 +1,66 @@
-local lspconfig = require("config.plugins").lspconfig
+local enabled = true
+
+local langservers = require("config.langservers")
+
+-- servers not managed by lspconfig
+local external = {
+	jdtls = true,
+}
+
+local opts = {
+	inlay_hints = { enabled = langservers.opts.hints },
+	codelens = { enabled = langservers.opts.lens },
+}
+
+local function config()
+	if not langservers.opts.enabled then
+		return false
+	end
+
+	local lspconfig = require("lspconfig")
+
+	local capabilities
+	if langservers.opts.completions then
+		local cmp = require("cmp_nvim_lsp")
+		capabilities = cmp.default_capabilities()
+	end
+
+	local server_name, server_opts
+	for _, server in ipairs(langservers.list) do
+		if not external[server.name] then
+			server_name = server.aliases.lspconfig or server.name
+
+			local lsopts = {}
+
+			if server.config then
+				server_opts = server.config.opts
+				if type(server_opts) == "function" then
+					lsopts = server_opts()
+				else
+					lsopts = server_opts
+				end
+
+				lsopts.capabilities = capabilities
+			end
+
+			if server.filetypes then
+				lsopts.filetypes = server.filetypes
+			end
+
+			lspconfig[server_name].setup(lsopts)
+		end
+	end
+
+	if langservers.opts.hints then
+		vim.lsp.inlay_hint.enable(true, { 0 })
+	end
+end
 
 return {
 	"neovim/nvim-lspconfig",
-	cond = lspconfig.enabled,
+	cond = enabled,
+	name = "lspconfig",
 	event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-	opts = lspconfig.config.opts,
-	config = lspconfig.config.setup,
+	config = config,
+	opts = opts,
 }
