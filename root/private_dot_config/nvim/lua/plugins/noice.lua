be7427a39b2a53086885ff6b7cfbf8ae22fd7002
shimeoki <shimeoki@gmail.com>
2025-04-26 12:48:35 +0300

    feat(nvim): no noice borders, skip some messages

--- root/private_dot_config/nvim/lua/plugins/noice.lua
+++ root/private_dot_config/nvim/lua/plugins/noice.lua
@@ -1,23 +1,95 @@
-local presets = {
-	command_palette = true,
-	long_message_to_split = true,
-	lsp_doc_border = true,
+local M = {}
+
+-- source: https://github.com/folke/noice.nvim/wiki/Configuration-Recipes
+
+local cmdline = {
+	format = {
+		input = { view = "cmdline_popup" }, -- don't force a border
+	},
+}
+
+local popup = {
+	border = {
+		style = "none",
+		padding = { 1, 1 },
+	},
+	filter_options = {},
+}
+
+local cmdline_popup = {
+	border = {
+		style = "none",
+		padding = { 1, 1 },
+	},
+	filter_options = {},
+	win_options = {
+		winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+	},
+}
+
+local notify = {
+	replace = true,
+}
+
+local hover = {
+	border = {
+		style = "none",
+		padding = { 1, 1 },
+	},
+	filter_options = {},
+	position = { row = 2, col = 0 },
+}
+
+local confirm = {
+	border = {
+		style = "none",
+		padding = { 1, 1 },
+	},
+	filter_options = {},
+}
+
+local views = {
+	popup = popup,
+	cmdline_popup = cmdline_popup,
+	notify = notify,
+	hover = hover,
+	confirm = confirm,
 }
 
 local lsp = {
 	override = {
 		["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 		["vim.lsp.util.stylize_markdown"] = true,
-		-- only for nvim-cmp
-		-- ["cmp.entry.get_documentation"] = true,
 	},
 }
 
-local opts = {
-	lsp = lsp,
-	presets = presets,
+local hide_search_virtual_text = {
+	filter = {
+		event = "msg_show",
+		kind = "search_count",
+	},
+	opts = { skip = true },
 }
 
-return {
-	opts = opts,
+local hide_written_messages = {
+	filter = {
+		event = "msg_show",
+		kind = "",
+		find = "written",
+	},
+	opts = { skip = true },
 }
+
+local routes = {
+	hide_search_virtual_text,
+	hide_written_messages,
+}
+
+M.opts = {
+	cmdline = cmdline,
+	lsp = lsp,
+	views = views,
+	routes = routes,
+}
+
+return M
