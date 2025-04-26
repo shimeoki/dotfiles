e40780198d01337139e9150345f36f60fe1f1880
shimeoki <shimeoki@gmail.com>
2025-04-26 12:48:01 +0300

    feat(nvim): set nvim-notify highlights
    overall, this is border removal

--- root/private_dot_config/nvim/lua/plugins/catppuccin.lua
+++ root/private_dot_config/nvim/lua/plugins/catppuccin.lua
@@ -7,10 +7,24 @@ local integrations = {
 	indent_blankline = { scope_color = "lavender" },
 }
 
-local function custom_highlights(colors)
+local function highlights(colors)
 	return {
+		-- luasnip
 		LuaSnipChoice = { fg = colors.peach, bg = colors.none },
 		LuaSnipInsert = { fg = colors.blue, bg = colors.none },
+		-- nvim-notify
+		NotifyBackground = { link = "NormalFloat" },
+		NotifyERRORBody = { link = "NormalFloat" },
+		NotifyWARNBody = { link = "NormalFloat" },
+		NotifyINFOBody = { link = "NormalFloat" },
+		NotifyDEBUGBody = { link = "NormalFloat" },
+		NotifyTRACEBody = { link = "NormalFloat" },
+		NotifyERRORBorder = { fg = colors.mantle, bg = colors.mantle },
+		NotifyWARNBorder = { fg = colors.mantle, bg = colors.mantle },
+		NotifyINFOBorder = { fg = colors.mantle, bg = colors.mantle },
+		NotifyDEBUGBorder = { fg = colors.mantle, bg = colors.mantle },
+		NotifyTRACEBorder = { fg = colors.mantle, bg = colors.mantle },
+		-- default split separator
 		WinSeparator = { fg = colors.base },
 	}
 end
@@ -20,7 +34,7 @@ local opts = {
 	background = { light = "latte", dark = "mocha" },
 	show_end_of_buffer = false,
 	integrations = integrations,
-	custom_highlights = custom_highlights,
+	custom_highlights = highlights,
 }
 
 return {
