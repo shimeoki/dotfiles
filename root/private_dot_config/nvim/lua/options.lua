0a8882b7ded950eef1d5cbcea77c88822f46a76b
shimeoki <shimeoki@gmail.com>
2025-05-07 10:22:40 +0300

    feat(nvim): add gtkcss for wofi

--- root/private_dot_config/nvim/lua/options.lua
+++ root/private_dot_config/nvim/lua/options.lua
@@ -93,6 +93,7 @@ vim.filetype.add({
 	pattern = {
 		[".*/.config/hypr/.*%.conf"] = "hyprlang",
 		[".*/.config/waybar/.*%.css"] = "gtkcss",
+		[".*/.config/wofi/.*%.css"] = "gtkcss",
 		[".*/.config/kitty/.*%.conf"] = "kitty",
 		[".*/.chezmoiignore"] = "gitignore",
 	},
