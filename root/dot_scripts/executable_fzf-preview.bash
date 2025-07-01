33f9e14649d5e5725839f87f1229286fbab4eebb
shimeoki <shimeoki@gmail.com>
2025-07-01 21:49:55 +0300

    feat(scripts): fzf-preview
    changes are listed in comments of the file

--- root/dot_scripts/executable_fzf-preview.bash
+++ root/dot_scripts/executable_fzf-preview.bash
@@ -4,33 +4,28 @@
 #
 # changes:
 #
-# - use kitty image protocol
-# - use bat
-# - formatting
+# - use only kitty image protocol
+# - use bat for files
+# - use eza for directories
+# - remove center (line number) and ignored
+# - remove sed hack
+# - remove comments
+# - don't specify bat style
+# - formatting, refactoring and styling
+# - more explicit extension
 
 if [[ $# -ne 1 ]]; then
-    >&2 echo "usage: $0 FILENAME[:LINENO][:IGNORED]"
+    >&2 echo "usage: $0 <file>"
     exit 1
 fi
 
 file=${1/#\~\//$HOME/}
-
-center=0
-if [[ ! -r $file ]]; then
-    if [[ $file =~ ^(.+):([0-9]+)\ *$ ]] &&
-        [[ -r ${BASH_REMATCH[1]} ]]; then
-
-        file=${BASH_REMATCH[1]}
-        center=${BASH_REMATCH[2]}
-    elif [[ $file =~ ^(.+):([0-9]+):[0-9]+\ *$ ]] &&
-        [[ -r ${BASH_REMATCH[1]} ]]; then
-
-        file=${BASH_REMATCH[1]}
-        center=${BASH_REMATCH[2]}
-    fi
-fi
-
 type=$(file --brief --dereference --mime -- "$file")
+
+if [[ $type =~ inode/ ]]; then
+    eza --color=always --tree --all --git-ignore -- "$file"
+    exit
+fi
 
 if [[ ! $type =~ image/ ]]; then
     if [[ $type =~ =binary ]]; then
@@ -38,9 +33,7 @@ if [[ ! $type =~ image/ ]]; then
         exit
     fi
 
-    bat --style="${BAT_STYLE:-numbers}" \
-        --color=always --pager=never \
-        --highlight-line="${center:-0}" -- "$file"
+    bat --color=always --pager=never -- "$file"
     exit
 fi
 
@@ -49,17 +42,5 @@ if [[ $dim = x ]]; then
     dim=$(stty size </dev/tty | awk '{print $2 "x" $1}')
 fi
 
-if [[ $KITTY_WINDOW_ID ]]; then
-    # 1. 'memory' is the fastest option but if you want the image to be
-    #    scrollable, you have to use 'stream'.
-    #
-    # 2. The last line of the output is the ANSI reset code without newline.
-    #    This confuses fzf and makes it render scroll offset indicator.
-    #    So we remove the last line and append the reset code to its previous
-    #    line.
-    kitten icat --clear --transfer-mode=memory \
-        --unicode-placeholder --stdin=no \
-        --place="$dim@0x0" "$file" | sed '$d' | sed $'$s/$/\e[m/'
-else
-    file "$file"
-fi
+kitten icat --clear --transfer-mode=memory --unicode-placeholder --stdin=no \
+    --place="$dim@0x0" -- "$file"
