439ed6085786651d4658d89c0540da211c9170a8
shimeoki <shimeoki@gmail.com>
2025-07-03 12:45:13 +0300

    feat(zsh): fzf.zsh rework
    - don't source fzf-git.zsh (not used at the moment)
    - use 100% height
    - use fzf-preview.bash

--- root/private_dot_config/zsh/fzf.zsh
+++ root/private_dot_config/zsh/fzf.zsh
@@ -5,27 +5,14 @@
 
 eval "$(fzf --zsh)"
 
-# todo: borrow opts from nushell config
+export FZF_DEFAULT_OPTS_FILE="$XDG_CONFIG_HOME/fzf/opts"
 
-# catppuccin mocha theme
-export FZF_DEFAULT_OPTS=" \
---color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
---color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
---color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
---color=selected-bg:#45475a \
---multi"
-
-FD_OPTS="--hidden --exclude .git"
-export FZF_DEFAULT_COMMAND="fd $FD_OPTS"
+export FZF_DEFAULT_COMMAND="fd --hidden --follow"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_ALT_C_COMMAND="fd --type=d $FD_OPTS"
+export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND --type=dir"
 
-FZF_DIR_PREVIEW="eza --tree --color=always {} | head -200"
-FZF_FILE_PREVIEW="bat -n --color=always --line-range :500 {}"
-FZF_PREVIEW="if [ -d {} ]; then $FZF_DIR_PREVIEW; else $FZF_FILE_PREVIEW; fi"
-
-export FZF_CTRL_T_OPTS="--preview '$FZF_PREVIEW'"
-export FZF_ALT_C_OPTS="--preview '$FZF_DIR_PREVIEW'"
+export FZF_CTRL_T_OPTS="--height=100% --preview='fzf-preview.bash {}'"
+export FZF_ALT_C_OPTS="$FZF_CTRL_T_OPTS"
 
 _fzf_compgen_path() {
     eval "$FZF_CTRL_T_COMMAND" . "$1"
@@ -40,11 +27,8 @@ _fzf_comprun() {
     shift
 
     case "$command" in
-    cd | j) fzf --preview "$FZF_DIR_PREVIEW" "$@" ;;
     export | unset) fzf --preview "eval 'echo \${}'" "$@" ;;
     ssh) fzf --preview 'dig {}' "$@" ;;
-    *) fzf --preview "$FZF_PREVIEW" "$@" ;;
+    *) fzf --preview 'fzf-preview.bash {}' "$@" ;;
     esac
 }
-
-. "$ZDOTDIR/fzf-git.zsh"
