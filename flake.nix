0b545fef02eaef14ad22169aefed922ab7377423
shimeoki <shimeoki@gmail.com>
2025-10-09 15:17:16 +0300

    feat(flake): add flake-parts

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        flake-parts.url = "github:hercules-ci/flake-parts";
         systems.url = "github:nix-systems/x86_64-linux";
     };
 
