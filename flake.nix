5b0ec939c14f932b02cc80581c112249d2f6ceae
shimeoki <shimeoki@gmail.com>
2025-10-09 17:05:39 +0300

    feat(flake): add treefmt

--- flake.nix
+++ flake.nix
@@ -3,8 +3,16 @@
 
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
         flake-parts.url = "github:hercules-ci/flake-parts";
         systems.url = "github:nix-systems/x86_64-linux";
+
+        treefmt = {
+            url = "github:numtide/treefmt-nix";
+            inputs = {
+                nixpkgs.follows = "nixpkgs";
+            };
+        };
     };
 
     outputs =
