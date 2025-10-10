5a9c25486c92b674041632e9080a15389a433e97
shimeoki <shimeoki@gmail.com>
2025-10-10 21:12:17 +0300

    style(nvim): treefmt

--- home/private_dot_config/nvim/luasnippets/go.lua
+++ home/private_dot_config/nvim/luasnippets/go.lua
@@ -1,20 +1,7 @@
 -- keep-sorted start block=yes newline_separated=yes
-local value = s("v", {
-	i(1, "v"),
-	t(" := "),
-	i(0),
-})
-
-local value_err = s("vr", {
-	i(1, "v"),
-	t(", err := "),
-	i(0),
-})
-
-local value_ok = s("vk", {
-	i(1, "v"),
-	t(", ok := "),
-	i(0),
+local address = s("a", {
+	t("&"),
+	i(0, "type"),
 })
 
 local array = s("arr", {
@@ -22,6 +9,117 @@ local array = s("arr", {
 	i(1, "..."),
 	t("]"),
 	i(0, "type"),
+})
+
+local equals = s("eq", {
+	t("== "),
+})
+
+local equals_nil = s("en", {
+	t("== nil"),
+})
+
+local even = s("even", {
+	i(1, "v"),
+	t("%2 == 0"),
+	i(0),
+})
+
+local if_block = s("ifb", {
+	t("if "),
+	i(1, "statement"),
+	t("; "),
+	i(2, "condition"),
+	t({ " {", "\t" }),
+	i(0),
+	t({ "", "}" }),
+})
+
+local if_nil = s("ifn", {
+	t("if "),
+	i(1, "v"),
+	t({ " == nil {", "\t" }),
+	i(0),
+	t({ "", "}" }),
+})
+
+local if_zero = s("ifz", {
+	t("if "),
+	i(1, "v"),
+	t({ " == 0 {", "\t" }),
+	i(0),
+	t({ "", "}" }),
+})
+
+local literal = s("lit", {
+	t("&"),
+	i(1, "type"),
+	t("{"),
+	i(0),
+	t("}"),
+})
+
+local logical_and = s("and", {
+	t("&& "),
+})
+
+local logical_not = s("not", {
+	t("!"),
+})
+
+local logical_or = s("or", {
+	t("|| "),
+})
+
+local not_equals = s("neq", {
+	t("!= "),
+})
+
+local not_equals_nil = s("nn", {
+	t("!= nil"),
+})
+
+local not_implemented = s("nimpl", {
+	t('panic("not implemented") // TODO: implement'),
+})
+
+local odd = s("odd", {
+	i(1, "v"),
+	t("%2 != 0"),
+	i(0),
+})
+
+local pointer = s("p", {
+	t("*"),
+	i(0, "type"),
+})
+
+local return_err = s("rr", {
+	t('return nil, errors.New("'),
+	i(0),
+	t('")'),
+})
+
+local return_nil = s("rn", {
+	t("return "),
+	i(0, "v"),
+	t(", nil"),
+})
+
+local slice = s("s", {
+	t("[]"),
+	i(0, "type"),
+})
+
+local todo = s("todo", {
+	t("// TODO: "),
+	i(0),
+})
+
+local value = s("v", {
+	i(1, "v"),
+	t(" := "),
+	i(0),
 })
 
 local value_array = s("varr", {
@@ -35,26 +133,10 @@ local value_array = s("varr", {
 	t("}"),
 })
 
-local slice = s("s", {
-	t("[]"),
-	i(0, "type"),
-})
-
-local value_slice = s("vs", {
+local value_err = s("vr", {
 	i(1, "v"),
-	t(" := []"),
-	i(2, "type"),
-	t("{"),
+	t(", err := "),
 	i(0),
-	t("}"),
-})
-
-local literal = s("lit", {
-	t("&"),
-	i(1, "type"),
-	t("{"),
-	i(0),
-	t("}"),
 })
 
 local value_literal = s("vlit", {
@@ -65,13 +147,6 @@ local value_literal = s("vlit", {
 	t("{"),
 	i(0),
 	t("}"),
-})
-
-local value_new = s("vnew", {
-	i(1, "v"),
-	t(" := new("),
-	i(0, "type"),
-	t(")"),
 })
 
 local value_make = s("vmake", {
@@ -94,135 +169,60 @@ local value_map = s("vmap", {
 	t("}"),
 })
 
-local address = s("a", {
-	t("&"),
+local value_new = s("vnew", {
+	i(1, "v"),
+	t(" := new("),
 	i(0, "type"),
+	t(")"),
 })
 
-local pointer = s("p", {
-	t("*"),
-	i(0, "type"),
-})
-
-local logical_and = s("and", {
-	t("&& "),
-})
-
-local logical_or = s("or", {
-	t("|| "),
-})
-
-local logical_not = s("not", {
-	t("!"),
-})
-
-local equals = s("eq", {
-	t("== "),
-})
-
-local not_equals = s("neq", {
-	t("!= "),
-})
-
-local equals_nil = s("en", {
-	t("== nil"),
-})
-
-local not_equals_nil = s("nn", {
-	t("!= nil"),
-})
-
-local even = s("even", {
+local value_ok = s("vk", {
 	i(1, "v"),
-	t("%2 == 0"),
+	t(", ok := "),
 	i(0),
 })
 
-local odd = s("odd", {
+local value_slice = s("vs", {
 	i(1, "v"),
-	t("%2 != 0"),
+	t(" := []"),
+	i(2, "type"),
+	t("{"),
 	i(0),
-})
-
-local if_nil = s("ifn", {
-	t("if "),
-	i(1, "v"),
-	t({ " == nil {", "\t" }),
-	i(0),
-	t({ "", "}" }),
-})
-
-local if_zero = s("ifz", {
-	t("if "),
-	i(1, "v"),
-	t({ " == 0 {", "\t" }),
-	i(0),
-	t({ "", "}" }),
-})
-
-local if_block = s("ifb", {
-	t("if "),
-	i(1, "statement"),
-	t("; "),
-	i(2, "condition"),
-	t({ " {", "\t" }),
-	i(0),
-	t({ "", "}" }),
-})
-
-local return_err = s("rr", {
-	t('return nil, errors.New("'),
-	i(0),
-	t('")'),
-})
-
-local return_nil = s("rn", {
-	t("return "),
-	i(0, "v"),
-	t(", nil"),
-})
-
-local not_implemented = s("nimpl", {
-	t('panic("not implemented") // TODO: implement'),
-})
-
-local todo = s("todo", {
-	t("// TODO: "),
-	i(0),
+	t("}"),
 })
 -- keep-sorted end
 
 return {
 	-- keep-sorted start
-	value,
-	value_err,
-	value_ok,
-	array,
-	value_array,
-	slice,
-	value_slice,
-	literal,
-	value_literal,
-	value_new,
-	value_make,
-	value_map,
 	address,
-	pointer,
-	logical_and,
-	logical_or,
-	logical_not,
+	array,
 	equals,
-	not_equals,
 	equals_nil,
-	not_equals_nil,
 	even,
-	odd,
+	if_block,
 	if_nil,
 	if_zero,
-	if_block,
+	literal,
+	logical_and,
+	logical_not,
+	logical_or,
+	not_equals,
+	not_equals_nil,
+	not_implemented,
+	odd,
+	pointer,
 	return_err,
 	return_nil,
-	not_implemented,
+	slice,
 	todo,
+	value,
+	value_array,
+	value_err,
+	value_literal,
+	value_make,
+	value_map,
+	value_new,
+	value_ok,
+	value_slice,
 	-- keep-sorted end
 }
