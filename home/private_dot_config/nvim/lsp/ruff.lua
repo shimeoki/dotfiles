32b92ce3d037a8dcaf7fd98f36f42ee68b3033e5
shimeoki <shimeoki@gmail.com>
2025-10-14 13:09:46 +0300

    feat(nvim): full ruff config

--- home/private_dot_config/nvim/lsp/ruff.lua
+++ home/private_dot_config/nvim/lsp/ruff.lua
@@ -1,4 +1,12 @@
+-- INFO: sources
+-- https://github.com/neovim/nvim-lspconfig/blob/ac98db2f9f06a56498ec890a96928774eae412c3/lsp/ruff.lua
+-- https://docs.astral.sh/ruff/editors/setup/#neovim
+
+--- @type vim.lsp.Config
 return {
+	cmd = { "ruff", "server" },
+	filetypes = { "python" },
+	root_markers = { "pyproject.toml", "ruff.toml", ".ruff.toml", ".git" },
 	-- disable hover
 	on_attach = function(client, _)
 		client.server_capabilities.hoverProvider = false
